4b5ec9ff46b7934e0d9ee64a33c77d3629a6d625
Pasha Kucherov <dev.kucherov@gmail.com>
2026-02-09 13:02:39 +0300

    сверстать шапку для секций

---
+++ src/section/Categories/Categories.jsx
@@ -0,0 +1,23 @@
+import './Categories.scss'
+import Section from "@/layouts/Section";
+
+const Categories = (props) => {
+  return (
+    <Section
+      title="Explore our wide variety of categories"
+      titleId="categories-title"
+      description="Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new"
+      actions={(
+        <div>
+          <button>back</button>
+          <button>forward</button>
+        </div>
+      )}
+      isActionHiddenOnMobile
+    >
+      CATEGORIES
+    </Section>
+  )
+}
+
+export default Categories
