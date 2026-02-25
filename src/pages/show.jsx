7124657ad8887eb01344ec75da92483f970ca368
Pasha Kucherov <dev.kucherov@gmail.com>
2026-02-25 16:07:44 +0300

    добавить страницу show, сверстать секцию ShowBanner

---
+++ src/pages/show.jsx
@@ -0,0 +1,13 @@
+import ShowBanner from "@/section/ShowBanner";
+
+export const metadata = {
+  title: 'Show - Stranger Things',
+}
+
+export default function () {
+  return (
+    <>
+      <ShowBanner />
+    </>
+  )
+}
