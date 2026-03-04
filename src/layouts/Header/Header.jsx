4a1581a3a1c315b92ddf03fb553d5d484cc6719c
Pasha Kucherov <dev.kucherov@gmail.com>
2026-03-04 11:23:08 +0300

    fix pages router

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -29,6 +29,10 @@ const Header = (props) => {
     },
   ]
 
+  const basePath = import.meta.env.MODE === 'production'
+    ? import.meta.env.VITE_PUBLIC_PATH
+    : ''
+
   return (
     <header
       className={classNames('header', {
@@ -56,7 +60,7 @@ const Header = (props) => {
                     className={classNames('header__menu-link', {
                       'is-active': href === url
                     })}
-                    href={href}
+                    href={`${basePath}${href}`}
                   >
                     {label}
                   </a>
